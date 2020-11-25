ccff7208dd0c25c2b71d0a25539e677763ed3bcb
HilesRenton <reneaaron.hilli@gmail.com>
2020-11-25 15:20:02 +0200

    Modified application properties

--- src/main/java/com/example/taskmanager/domain/User.java
+++ src/main/java/com/example/taskmanager/domain/User.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 
 
@@ -15,6 +16,7 @@ import javax.persistence.Id;
 
 
 @Entity
+@Table(name="usertable")
 public class User {
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
